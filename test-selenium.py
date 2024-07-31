bc335ac0d13bc1b998f2671e26cca4dbfe42d79b
letanloc-tlooc <loclemap@gmail.com>
2024-07-31 00:44:29 -0700

    update

--- test-selenium.py
+++ test-selenium.py
@@ -1,17 +1,35 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
 import time
-# Khởi động Firefox WebDriver
-driver = webdriver.Firefox()
+
+# Thiết lập tùy chọn cho Firefox
+options = Options()
+# options.headless = True  # Uncomment this line to run Firefox in headless mode (no GUI)
+
+# Cấu hình Geckodriver
+service = Service(executable_path='/usr/bin/geckodriver')
+
+# Khởi động Firefox WebDriver với các tùy chọn
+driver = webdriver.Firefox(service=service, options=options)
 
 try:
-    # Truy cập trang đăng nhập GitHub
+    # Truy cập trang YouTube
     driver.get("https://www.youtube.com/")
-    # Đợi một vài giây để quá trình tạo repository hoàn tất
-    time.sleep(10)
+    
+    # Đợi trang tải xong
+    WebDriverWait(driver, 10).until(EC.title_contains("YouTube"))
+    
+    # Thực hiện các thao tác trên trang, nếu cần
+
+
+    # Đợi kết quả tìm kiếm xuất hiện
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'contents')))
+    
 finally:
     # Đóng trình duyệt
     driver.quit()
