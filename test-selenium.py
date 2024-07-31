d245e2faf9cf12d792aa2010d25c7c42d4328d38
letanloc-tlooc <loclemap@gmail.com>
2024-07-31 00:54:56 -0700

    update

--- test-selenium.py
+++ test-selenium.py
@@ -7,17 +7,17 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 import time
 
-driver = webdriver.Firefox()
+# driver = webdriver.Firefox()
 
 # # Thiết lập tùy chọn cho Firefox
-# options = Options()
+options = Options()
 # # options.headless = True  # Uncomment this line to run Firefox in headless mode (no GUI)
 
 # # Cấu hình Geckodriver
-# service = Service(executable_path='/usr/bin/geckodriver')
+service = Service(executable_path='/usr/bin/geckodriver')
 
 # # Khởi động Firefox WebDriver với các tùy chọn
-# driver = webdriver.Firefox(service=service, options=options)
+driver = webdriver.Firefox(service=service, options=options)
 
 try:
     # Truy cập trang YouTube
